fc553712812ff5929ceef512aa932c76883b033b
Erdem Bircan <erdem.erdembircan@gmail.com>
2026-08-26 11:50:47 +0300

    [FEAT]: add pattern containment engine

---
+++ src/algebra/index.ts
@@ -0,0 +1,19 @@
+export { analyze } from "./analyze.js";
+export { subsumes } from "./subsumes.js";
+export type {
+  Analysis,
+  AnalyzeOptions,
+  ClassMember,
+  ClassMemberChar,
+  ClassMemberRange,
+  Issue,
+  IssueKind,
+  Segment,
+  SegmentGlobstar,
+  SegmentTokens,
+  Token,
+  TokenClass,
+  TokenLit,
+  TokenQuestion,
+  TokenStar,
+} from "./types.js";
